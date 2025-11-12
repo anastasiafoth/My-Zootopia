9f6c91de0374343d172a56017fe88a3938c80cc2
Anastasia Foth <anastasiafoth@Mac.localdomain>
2025-11-12 09:14:05 +0100

    exported the serialization of a single animal object to a different function

--- animals_web_generator.py
+++ animals_web_generator.py
@@ -7,31 +7,37 @@ def load_data(file_path):
 
 animals_data = load_data("animals_data.json")
 
+
+def serialize_animal(animal):
+    output = ""
+    name = animal.get("name")
+    diet = animal.get("characteristics").get("diet")
+    location = animal.get("locations")[0]
+    type_ = animal.get("characteristics").get("type")
+
+    output += '<li class="cards__item">\n'
+
+    if name:
+        output += f'<div class="card__title">{name}</div>\n'
+
+    output += '<p class="card__text">\n'
+    if diet:
+        output += f"<strong>Diet:</strong> {diet}<br/>\n"
+
+    if location:
+        output += f"<strong>Location:</strong> {location}<br/>\n"
+
+    if type_:
+        output += f"<strong>Type:</strong> {type_}<br/>\n"
+
+    output += '</p>\n </li>\n'
+    return output
+
 def print_data(data):
     output = ""
     for animal in data:
-        # append information to each string
-        name = animal.get("name")
-        diet = animal.get("characteristics").get("diet")
-        location = animal.get("locations")[0]
-        type_ = animal.get("characteristics").get("type")
-
-        output += '<li class="cards__item">'
-
-        if name:
-            output += f'<div class="card__title">{name}</div>\n'
-
-        output += '<p class="card__text">\n'
-        if diet:
-            output += f"<strong>Diet:</strong> {diet}<br/>\n"
-
-        if location:
-            output += f"<strong>Location:</strong> {location}<br/>\n"
-
-        if type_:
-            output += f"<strong>Type:</strong> {type_}<br/>\n"
-
-        output += '</p>\n </li>\n'
+        # append information to string
+        output += serialize_animal(animal)
     return output
 
 
@@ -42,7 +48,6 @@ def load_html(file_path):
 
 output = print_data(animals_data)
 html = load_html("animals_template.html")
-
 html = html.replace("__REPLACE_ANIMALS_INFO__", output)
 
 with open("animals_template.html", "w") as file:
