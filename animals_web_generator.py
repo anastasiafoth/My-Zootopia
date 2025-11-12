7307fb9fb8b05fe194369a2434e1d1bd5b388447
anastasiafoth <anastasiafoth9@gmail.com>
2025-11-12 14:29:36 +0100

    scripts generates the website only for the animals with the selected skin_type

--- animals_web_generator.py
+++ animals_web_generator.py
@@ -16,6 +16,7 @@ def serialize_animal(animal):
     type_ = animal.get("characteristics").get("type")
     slogan = animal.get("characteristics").get("slogan")
     color = animal.get("characteristics").get("color")
+    skin_type = animal.get("characteristics").get("skin_type")
 
     output += "<li class='cards__item'>\n"
 
@@ -38,14 +39,28 @@ def serialize_animal(animal):
     if color:
         output += f"<li class='list_enumeration'><strong>Color:</strong> {color}</li>\n"
 
+    if skin_type:
+        output += f"<li class='list_enumeration'><strong>Skin Type:</strong> {skin_type}</li>\n"
+
     output += '</ul>\n</div>\n </li>\n'
     return output
 
-def print_data(data):
-    output = ""
+
+def skin_types_in_list(data):
+    skin_types_list = []
     for animal in data:
-        # append information to string
-        output += serialize_animal(animal)
+        skin_type = animal.get("characteristics").get("skin_type")
+        if skin_type and skin_type not in skin_types_list:
+            skin_types_list.append(skin_type)
+    return skin_types_list
+
+
+def return_data_based_on_skin_type(skin_type_input):
+    output = ""
+
+    for animal in animals_data:
+        if skin_type_input == animal.get("characteristics").get("skin_type").lower():
+            output += serialize_animal(animal)
     return output
 
 
@@ -54,10 +69,37 @@ def load_html(file_path):
         return file.read()
 
 
-output = print_data(animals_data)
-html = load_html("animals_template.html")
-html = html.replace("__REPLACE_ANIMALS_INFO__", output)
+def write_html(skin_type_input):
+    output = return_data_based_on_skin_type(skin_type_input)
 
-with open("animals_template.html", "w") as file:
-    file.write(html)
+    html = load_html("animals_template.html")
+    html = html.replace("__REPLACE_ANIMALS_INFO__", output)
 
+    with open("animals_template.html", "w") as file:
+        file.write(html)
+
+
+def main():
+    skin_types_list = skin_types_in_list(animals_data)
+    skin_types_as_string = ", ".join(skin_types_list)
+
+    while True:
+        skin_type_input = input(f"Here are the skin types of foxes: \n{skin_types_as_string}\nPlease enter one: ").lower().strip()
+        if skin_type_input.isdigit():
+            print("Please enter a string!")
+        elif not skin_type_input:
+            print("Input shouldn´t be empty!")
+        elif skin_type_input:
+            for skin_type in skin_types_list:
+                if skin_type.lower() == skin_type_input:
+                    break
+            else:
+                print("Skin type not found!")
+
+            break
+
+    write_html(skin_type_input)
+
+
+if __name__ == "__main__":
+    main()
