ad10f6b88e05fff4caaa7b95545ae06822a25f57
anastasiafoth <anastasiafoth9@gmail.com>
2025-11-25 15:44:51 +0100

    Exported API Key to .env file and added .gitignore

--- data_fetcher.py
+++ data_fetcher.py
@@ -1,6 +1,10 @@
 import requests
+import os
+from dotenv import load_dotenv
 
-API_KEY = "igVwTHLCXTvcKZ9bA+Ujtg==0tNq4XKjwH8DoWSY"
+load_dotenv()
+API_KEY = os.getenv("API_KEY")
+
 URL = "https://api.api-ninjas.com/v1/animals?name="
 
 def load_data_from_api(user_input):
